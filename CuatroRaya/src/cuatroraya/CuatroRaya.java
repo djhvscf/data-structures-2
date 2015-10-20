0cf3f56228732856edcb5160c115e949ece5920d
Dennis Hernández <dennishernandezvargas@gmail.com>
2015-10-19 21:23:31 -0600

    improving the code

--- CuatroRaya/src/cuatroraya/CuatroRaya.java
+++ CuatroRaya/src/cuatroraya/CuatroRaya.java
@@ -2,19 +2,19 @@ package cuatroraya;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 import javax.swing.*;
 
 public class CuatroRaya extends Frame implements MouseListener, ActionListener {
 
-    JButton Comienza = new JButton("Recomenzar");
-    JButton btnModo = new JButton("Dos Jugador");
+    JButton Comienza = new JButton("Nuevo Juego");
+    JButton Modo = new JButton("Dos Jugadores");
     JButton Salir = new JButton("Salir");
-    Label Vez = new Label("Turno:");
-    Label Quien = new Label("Patético Humano1");
-    Label Victorias = new Label("Victorias: 0");
-    Label Derrotas = new Label("Derrotas: 0");
-    Label Jugadas = new Label("Jugadas: 0");
-    int estados[][] = new int[7][7];
+    JLabel Quien = new JLabel("Jugador 1");
+    JLabel Victorias = new JLabel("Victorias: 0");
+    JLabel Derrotas = new JLabel("Derrotas: 0");
+    JLabel Jugadas = new JLabel("Jugadas: 0");
+    int[][] tablero = new int[7][7];
     int Turno;
     int victorias;
     int derrotas;
@@ -23,19 +23,18 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
     Panel PanelPrincipal = new Panel();
 
     CuatroRaya() {
-        super("Cuatro en Raya");
+        super("4 en Raya");
         setLayout(new GridLayout(20, 5));
         addMouseListener(this);
         Comienza.addMouseListener(this);
         Salir.addMouseListener(this);
-        btnModo.addActionListener(this);
-        PanelPrincipal.add(Vez);
+        Modo.addActionListener(this);
         PanelPrincipal.add(Quien);
         PanelPrincipal.add(Victorias);
         PanelPrincipal.add(Derrotas);
         PanelPrincipal.add(Jugadas);
         PanelPrincipal.add(Comienza);
-        PanelPrincipal.add(btnModo);
+        PanelPrincipal.add(Modo);
         PanelPrincipal.add(Salir);
         add(PanelPrincipal);
         victorias = 0;
@@ -56,45 +55,38 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         int result;
         int z = 0;
         int y;
+        
         for (z = 0; z < 7; z++) {
-            if (estados[x][z] != 0) {
+            if (tablero[x][z] != 0) {
                 break;
             }
         }
-        if (z == 0) {
-            y = 7;
-        } else {
-            y = z - 1;
-        }
+        
+        y = z == 0 ? 7 : (z - 1);
 
-        if ((y == 6 && estados[x][y] == 0) || ((y < 6) && (estados[x][y + 1] != 0) && (estados[x][y] == 0))) {
+        if ((y == 6 && tablero[x][y] == 0) || ((y < 6) && (tablero[x][y + 1] != 0) && (tablero[x][y] == 0))) {
             if (Turno % 2 == 0) {
                 g.setColor(Color.red);
-                estados[x][y] = -1;
-                Quien.setText("Maquina");
-                if (!unjugador) {
-                    Quien.setText("Patético Humano 2");
-                }
+                tablero[x][y] = -1;
+                Quien.setText(unjugador ? "PC" : "Jugador 2");
             } else {
-                g.setColor(Color.blue);
-                estados[x][y] = 1;
-                Quien.setText("Patético Humano 1");
+                g.setColor(Color.YELLOW);
+                tablero[x][y] = 1;
+                Quien.setText("Jugador 1");
             }
             g.fillOval((x * 90) + 20, (y * 80) + 100, 70, 70);
             Turno++;
-            Jugadas.setText("Jugada: " + Integer.toString(Turno));
+            Jugadas.setText("Jugadas: " + Integer.toString(Turno));
 
             if (CompruebaCasilla(x, y, Resultados)) {
-                Ganaste(estados[x][y]);
+                Ganaste(tablero[x][y]);
             } else {
                 if (Turno % 2 != 0) {
                     if (unjugador) {
                         result = JuegaIA(0);
                         if (result >= 10) {
-                            System.out.println("la IA decidió: " + Integer.toString((result / 10) - 1));
                             PintaCasilla((result / 10) - 1);
                         } else {
-                            System.out.println("la IA decidió: " + Integer.toString(result));
                             PintaCasilla(result);
                         }
                     }
@@ -102,7 +94,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
             }
         }
     }
-
+    
     public boolean CompruebaCasilla(int posx, int posy, int cuantas[][]) {
         int x, y;
         for (x = (-1); x < 2; x++) {
@@ -121,11 +113,11 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
     public int CompruebaLinea(int posx, int posy, int x, int y) {
         int tempx, tempy;
         int cuantas = 1;
-        int jugador = estados[posx][posy];
+        int jugador = tablero[posx][posy];
         tempx = posx + x;
         tempy = posy + y;
         while ((tempy >= 0 && tempx >= 0) && (tempx < 7 && tempy < 7)) {
-            if (estados[tempx][tempy] == jugador) {
+            if (tablero[tempx][tempy] == jugador) {
                 cuantas++;
                 tempx += x;
                 tempy += y;
@@ -136,7 +128,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         tempx = posx - x;
         tempy = posy - y;
         while ((tempy >= 0 && tempx >= 0) && (tempx < 7 && tempy < 7)) {
-            if (estados[tempx][tempy] == jugador) {
+            if (tablero[tempx][tempy] == jugador) {
                 cuantas++;
                 tempx -= x;
                 tempy -= y;
@@ -155,12 +147,12 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         int x, y, casilla;
         int z;
         boolean mayor;
-        //System.out.println("entró en JuegaIA, Turno: " + Integer.toString(Turno));
+        
         //Recorreré cada casilla en busca de la mejor opción
         for (casilla = 0; casilla < 7; casilla++) {
             //calculo la y actual para cada casilla
             z = 0;
-            while (estados[casilla][z] == 0) {
+            while (tablero[casilla][z] == 0) {
                 z++;
                 if (z == 7) {
                     break;
@@ -170,7 +162,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
                 y = 6;
             } else {
                 y = z - 1;
-                //System.out.println("IA analizando x: " + Integer.toString(casilla) + "  y: "+Integer.toString(y));
+                
                 //Una casilla recibe puntos por altura, un factor semialeatorio, para evitar
                 //bucles infinitos
                 PuntosCasilla[casilla] += y;
@@ -178,13 +170,13 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
                 PuntosCasilla[casilla] += Nveces % (casilla + 1);
 
                 //Obtengo mis valores para esa jugada simulando la colocación de una ficha
-                estados[casilla][y] = 1;
+                tablero[casilla][y] = 1;
                 CompruebaCasilla(casilla, y, ValoresYo);
 
-                estados[casilla][y] = -1;
-                //Obtengo los resultados que el tendríoa con esa jugada simulando
+                tablero[casilla][y] = -1;
+                //Obtengo los resultados que el tendría con esa jugada simulando
                 CompruebaCasilla(casilla, y, ValoresEl);
-                estados[casilla][y] = 0;
+                tablero[casilla][y] = 0;
 
                 //valoro mucho si yo gano(1000) o si el gana(100)
                 for (x = 0; x < 3; x++) {
@@ -196,50 +188,47 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
                             PuntosCasilla[casilla] += 100;
                         }
                         if (ValoresYo[x][z] == 3 && Nveces < 3) {
-                            estados[casilla][y] = 1;
-                            System.out.println("Llamando a IA Recursiva en la vez: " + Nveces);
+                            tablero[casilla][y] = 1;
+
                             if (JuegaIA(Nveces + 1) > 10) {
                                 PuntosCasilla[casilla] += 90;
                             } else {
                                 PuntosCasilla[casilla] += 5;
                             }
 
-                            estados[casilla][y] = 0;
+                            tablero[casilla][y] = 0;
                         }
                         if (ValoresEl[x][z] == 3 && Nveces < 3) {
-                            estados[casilla][y] = (-1);
-                            System.out.println("Llamando a IA Recursiva en la vez: " + Nveces);
+                            tablero[casilla][y] = (-1);
+                            
                             if (JuegaIA(Nveces + 1) > 10) {
                                 PuntosCasilla[casilla] += 100;
                             } else {
                                 PuntosCasilla[casilla] += 10;
                             }
-                            estados[casilla][y] = 0;
+                            tablero[casilla][y] = 0;
                         }
                         if (ValoresYo[x][z] == 2) {
                             PuntosCasilla[casilla] += 5;
                         }
                     }
                 }
-                //Compruebo la casilla de encima Tanto de él como yo
+                
                 if (y > 0) {
-                    estados[casilla][y - 1] = (1);
+                    tablero[casilla][y - 1] = (1);
                     CompruebaCasilla(casilla, y - 1, ValoresYo);
 
-                    estados[casilla][y - 1] = (-1);
+                    tablero[casilla][y - 1] = (-1);
                     CompruebaCasilla(casilla, y - 1, ValoresEl);
-                    estados[casilla][y - 1] = 0;
+                    tablero[casilla][y - 1] = 0;
                 }
-                //valoro si el gana poniendo encima de mi o si me jodo
+                
                 for (x = 0; x < 3; x++) {
                     for (z = 0; z < 3; z++) {
-                        //Me jodería una de victoria y eso no mola
                         if (ValoresYo[x][z] >= 4) {
                             PuntosCasilla[casilla] -= 10;
                         }
 
-                        //Le daría la victoria y eso solo lo puedo permitir
-                        //si gano con esa jugada(o sea con 1000 de antes)
                         if (ValoresEl[x][z] >= 4) {
                             PuntosCasilla[casilla] -= 100;
                         }
@@ -252,7 +241,6 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         //así que vamos a ver cual tiene mayor puntuación y colocar en ella
         z = 0;
         for (y = 0; y < 7; y++) {
-        //System.out.println("La opción: "+Integer.toString(y) + " Recibe "+Integer.toString(PuntosCasilla[y])+ " Puntos");
             mayor = true;
             for (x = 0; x < 7; x++) {
                 if (PuntosCasilla[y] < PuntosCasilla[x]) {
@@ -267,8 +255,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
                 }
             }
         }
-
-        System.out.println("Final de JuegaIA");
+        System.out.println("" + Arrays.deepToString(ValoresEl));
         return (z);
     }
     //* *************************LÓGICA PC**************************
@@ -278,7 +265,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         if (jugador == (-1)) {
             victorias++;
             if (unjugador) {
-                s = "GANASTE JUGADOR HUMANO";
+                s = "GANASTE JUGADOR 1";
                 Victorias.setText("Victorias: " + Integer.toString(victorias));
                 Derrotas.setText("Derrotas: " + Integer.toString(derrotas));
             } else {
@@ -289,7 +276,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         } else {
             derrotas++;
             if (unjugador) {
-                s = "GANÓ LA MÁQUINA!!!";
+                s = "GANÓ LA PC";
                 Victorias.setText("Victorias: " + Integer.toString(victorias));
                 Derrotas.setText("Derrotas: " + Integer.toString(derrotas));
             } else {
@@ -326,7 +313,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         for (x = 0; x < 7; x++) {
             for (y = 0; y < 7; y++) {
                 g.fillOval((x * 90) + 20, (y * 80) + 100, 70, 70);
-                estados[x][y] = 0;
+                tablero[x][y] = 0;
             }
         }
     }
@@ -337,7 +324,6 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         int y;
         Turno = 0;
         jugando = true;
-        System.out.println("reestart");
         g.setColor(Color.lightGray);
         g.fillRect(1, 1, 750, 690);
 
@@ -347,7 +333,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         for (x = 0; x < 7; x++) {
             for (y = 0; y < 7; y++) {
                 g.fillOval((x * 90) + 20, (y * 80) + 100, 70, 70);
-                estados[x][y] = 0;
+                tablero[x][y] = 0;
             }
         }
     }
@@ -356,7 +342,7 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
         CuatroRaya cuatroRaya = new CuatroRaya();
         cuatroRaya.pack();
         cuatroRaya.setSize(750, 690);
-        //cuatroRaya.setResizable(false);
+        cuatroRaya.setResizable(false);
         cuatroRaya.setVisible(true);
         cuatroRaya.ReStart();
         
@@ -364,18 +350,19 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == btnModo) {
-            if ("Un Jugador".equals(btnModo.getText())) {
+        if (ae.getSource() == Modo) {
+            if ("Un Jugador".equals(Modo.getText())) {
                 Victorias.setText("Victorias: " + Integer.toString(victorias));
                 Derrotas.setText("Derrotas: " + Integer.toString(derrotas));
-                btnModo.setText("Dos Jugadores");
+                Modo.setText("Dos Jugadores");
                 unjugador = true;
             } else {
                 Victorias.setText("Victorias J1: " + Integer.toString(victorias));
                 Derrotas.setText("Victorias J2: " + Integer.toString(derrotas));
-                btnModo.setText("Un Jugador");
+                Modo.setText("Un Jugador");
                 unjugador = false;
             }
+            this.ReStart();
         }
     }
 
@@ -383,9 +370,8 @@ public class CuatroRaya extends Frame implements MouseListener, ActionListener {
     public void mousePressed(MouseEvent me) {
         int z;
         z = me.getX();
-        System.out.println("evento click");
+        
         if (jugando) {
-            System.out.println("evento click jugando");
             z = (z - 10) / 90;
             PintaCasilla(z);
         }
@@ -423,7 +409,6 @@ class CuadroDialogo extends Dialog {
 
     Button aceptar = new Button("OK");
     Label texto = new Label();
-    String cad;
 
     CuadroDialogo(Frame f, String s) {
         super(f, "informacion");
@@ -438,7 +423,7 @@ class CuadroDialogo extends Dialog {
     @Override
     public void paint(Graphics g) {
         g.setColor(Color.white);
-        g.drawString(cad, 50, 50);
+        g.drawString("paint", 50, 50);
     }
 
     @Override
